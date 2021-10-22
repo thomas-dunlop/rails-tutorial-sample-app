4c61dd6d79ea26a00f189ae7cc0eaf128c054ac7
thomas-dunlop <dunlopt12@gmail.com>
2021-10-22 08:38:29 -0400

    Minor change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => 'apikey'
+  :user_name => 'apikey',
   :password => ENV['SENDGRID_API_KEY'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
