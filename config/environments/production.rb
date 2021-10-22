7b2d10dbfd0b26f0f0aceb02f452bb523ce5d9b9
thomas-dunlop <dunlopt12@gmail.com>
2021-10-22 08:51:10 -0400

    Email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,13 @@ Rails.application.configure do
   host = 'my-rails-tutorial-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => 'apikey',
-  :password => ENV['SENDGRID_API_KEY'],
-  :domain => 'heroku.com',
-  :enable_starttls_auto => true
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
